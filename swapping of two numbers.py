fa2e8b4db33688d8e99d20b7a6bfbbc8bd07bd02
gsk1307 <132924253+gsk1307@users.noreply.github.com>
2023-05-24 15:30:40 +0530

    Add files via upload
    OUTPUT:
    enter num1 :216
    enter num2 :302
    302 is value of a and 216 is value of b after swaping
    302 216

---
+++ swapping of two numbers.py
@@ -0,0 +1,7 @@
+#swapping of two numbers
+a=int(input("enter num1 :"))
+b=int(input("enter num2 :"))
+a,b=b,a #a is assinged to b and viseversa
+print(f'{a} is value of num1 and {b} is value of num2 after swaping')
+#or
+print(a,b)
